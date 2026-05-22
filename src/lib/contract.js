f1bb9ac2ee0c4d5925fd3518f22dd1f9fdfac44c
sadikuernest00-prog <sadikuernest00@gmail.com>
2026-05-22 18:20:11 +0200

    Create contract.js

---
+++ src/lib/contract.js
@@ -0,0 +1,119 @@
+import { ethers } from "ethers";
+
+export const contractAddress =
+  "0x6B4e40CA4A4C86541333aAAAEfd360a6CC3885bd";
+
+export const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "address payable",
+        "name": "_seller",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "buyer",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "deposit",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "isFunded",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "isReleased",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "releaseFunds",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "seller",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+export async function getContract() {
+
+  if (!window.ethereum) {
+    alert("Please install MetaMask");
+    return;
+  }
+
+  await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+  const provider = new ethers.BrowserProvider(window.ethereum);
+
+  const signer = await provider.getSigner();
+
+  return new ethers.Contract(
+    contractAddress,
+    abi,
+    signer
+  );
+}
