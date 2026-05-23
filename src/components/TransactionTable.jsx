b6e80184e85f8e20a0f5f7eb28751fc0bfa22d68
sadikuernest00-prog <sadikuernest00@gmail.com>
2026-05-23 14:33:57 +0200

    Update TransactionTable.jsx

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,1 +1,26 @@
 
+export default function TransactionTable({
+  transactions,
+}) {
+  return (
+    <table className="w-full text-white">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Amount</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map((tx) => (
+          <tr key={tx.id}>
+            <td>{tx.id}</td>
+            <td>${tx.amount}</td>
+            <td>{tx.status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
